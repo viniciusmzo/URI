6284c658c6bc3cfbc636144012adc5d395cb4b37
Vinicius <vinicius_mzo@hotmail.com>
2019-07-17 16:31:18 -0300

    Exercicio URI 1003

---
+++ 1003/1003/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _1003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, soma;
+
+            a = int.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            b = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            soma = a + b;
+            Console.WriteLine("SOMA = " + soma.ToString(CultureInfo.InvariantCulture));
+            Console.ReadKey();
+        }
+    }
+}
