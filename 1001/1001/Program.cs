635f0133055ee4d053a67afc29046f34e1937b23
Vinicius <vinicius_mzo@hotmail.com>
2019-07-17 16:12:54 -0300

    Exercicio URI 1001

---
+++ 1001/1001/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _1001
+{
+    class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            int a, b, x;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            x = a + b;
+
+            Console.WriteLine("X = " + x);
+            Console.ReadKey();
+           
+        
+
+
+
+        }
+    }
+}
