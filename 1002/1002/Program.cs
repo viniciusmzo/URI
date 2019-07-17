a00106a9f7eedb0b49f14b3bee05107867cf5d35
Vinicius <vinicius_mzo@hotmail.com>
2019-07-17 16:15:14 -0300

    Exercicio URI 1002

---
+++ 1002/1002/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _1002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n, raio, area;
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            n = 3.14159;
+            area = Math.Pow(raio, 2) * n;
+
+
+            Console.Write("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
+            Console.ReadKey();
+        }
+    }
+}
