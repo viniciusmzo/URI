c97869cf119d20121bbbc59ad715c75118e58954
Vinicius <vinicius_mzo@hotmail.com>
2019-07-17 23:36:44 -0300

    Exercicio URI 1004

---
+++ 1004/1004/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _1004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor1, valor2, produto;
+
+            valor1 = int.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            valor2 = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            produto = valor1 * valor2;
+            Console.WriteLine("PROD = " + produto.ToString(CultureInfo.InvariantCulture));
+            Console.ReadKey();
+
+        }
+    }
+}
