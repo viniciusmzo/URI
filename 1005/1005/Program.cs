3e28e716b65b41a390d7755ab1290f18ec24385f
Vinicius <vinicius_mzo@hotmail.com>
2019-07-17 23:54:36 -0300

    Exercicio URI 1005

---
+++ 1005/1005/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _1005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2, media;
+
+            nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            media = (((nota1 * 3.5) + (nota2 * 7.5)) /11);
+            Console.WriteLine("MEDIA = " + media.ToString("F5", CultureInfo.InvariantCulture));
+            Console.ReadKey();
+
+        }
+    }
+}
