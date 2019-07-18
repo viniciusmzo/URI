5be9b50055b355dc896ad580d3afefafe29e7c86
Vinicius <vinicius_mzo@hotmail.com>
2019-07-18 16:30:12 -0300

    Exercicio URI 1011

---
+++ 1011/1011/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _1011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi, raio, volume;
+
+            pi = 3.14159;
+
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            volume = (4.0 / 3.0) * pi * (Math.Pow(raio, 3));
+
+            Console.WriteLine("VOLUME = " + volume.ToString("F3", CultureInfo.InvariantCulture));
+            Console.ReadKey();
+        }
+    }
+}
