615044ba99fe415ecd34be937ab597762f606124
Vinicius <vinicius_mzo@hotmail.com>
2019-07-18 09:24:05 -0300

    Exercicio URI 1008

---
+++ 1008/1008/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _1008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numFuncionario, horasTrabalhadas;
+            double valorHora;
+
+            
+            numFuncionario = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            horasTrabalhadas = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+          
+            valorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            valorHora = valorHora * horasTrabalhadas;
+
+            Console.WriteLine("NUMBER = " + numFuncionario);
+            Console.WriteLine("SALARY = U$ " + valorHora.ToString("F2", CultureInfo.InvariantCulture));
+            Console.ReadKey();
+
+        }
+    }
+}
