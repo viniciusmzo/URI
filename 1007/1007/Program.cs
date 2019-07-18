ccc5159cbad10e66ba6eb081424c1a085c93adca
Vinicius <vinicius_mzo@hotmail.com>
2019-07-18 09:04:13 -0300

    Exercicio URI 1007

---
+++ 1007/1007/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _1007
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, diferenca;
+
+            a = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            b = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            d = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            diferenca = ((a * b) - (c * d));
+
+            Console.WriteLine("DIFERENCA = " + diferenca.ToString(CultureInfo.InvariantCulture));
+            Console.ReadKey();
+        }
+    }
+}
