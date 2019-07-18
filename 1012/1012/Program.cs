cf66d02a6f3b05cca050b5c336a264c3077ddcf6
Vinicius <vinicius_mzo@hotmail.com>
2019-07-18 16:54:31 -0300

    Exercicio URI 1012

---
+++ 1012/1012/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _1012
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            double pi = 3.14159;
+            string[] vet = Console.ReadLine().Split(' ');
+
+            a = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            b = double.Parse(vet[1], CultureInfo.InvariantCulture);
+            c = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            double areaTriangulo = (a * c) / 2;
+            double areaCirculo = pi * Math.Pow(c, 2);
+            double areaTrapezio = ((a + b) * c) / 2;
+            double areaQuadrado = b * b;
+            double areaRetangulo = a * b;
+
+            Console.WriteLine("TRIANGULO: " + areaTriangulo.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("CIRCULO: " + areaCirculo.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("TRAPEZIO: " + areaTrapezio.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("QUADRADO: " + areaQuadrado.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("RETANGULO: " + areaRetangulo.ToString("F3", CultureInfo.InvariantCulture));
+
+            Console.ReadKey();
+        }
+    }
+}
