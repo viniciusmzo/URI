0f1ad105f210195053ca56f6530316c3168db3bd
Vinicius <vinicius_mzo@hotmail.com>
2019-07-19 11:40:33 -0300

    Exercicio URI 1017

---
+++ 1017/1017/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _1017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tempo, kmMedia;
+            double consumo;
+
+            tempo = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            kmMedia = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            consumo = (tempo * kmMedia) / 12.0;
+
+            Console.WriteLine(consumo.ToString("F3", CultureInfo.InvariantCulture));
+            Console.ReadKey();
+        }
+    }
+}
