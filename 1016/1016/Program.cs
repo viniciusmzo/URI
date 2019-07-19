4828e06522638134b1845690cb1c2a2016439907
Vinicius <vinicius_mzo@hotmail.com>
2019-07-19 11:29:26 -0300

    Exercicio URI 1016

---
+++ 1016/1016/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _1016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int km, tempo;
+
+            km = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            tempo = km * 2;
+
+            Console.WriteLine(tempo.ToString("F0", CultureInfo.InvariantCulture) + " minutos");
+            Console.ReadKey();
+
+        }
+    }
+}
