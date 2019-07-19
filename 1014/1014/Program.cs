5ba5abae938ea81dc452c636490a6bb64ac0762e
Vinicius <vinicius_mzo@hotmail.com>
2019-07-18 22:41:57 -0300

    Exercicio URI 1014

---
+++ 1014/1014/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _1014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int km;
+            double litros;
+
+            km = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            litros = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double autonomia = km / litros;
+
+            Console.WriteLine(autonomia.ToString("F3", CultureInfo.InvariantCulture) + " km/l");
+            Console.ReadKey();
+        }
+    }
+}
