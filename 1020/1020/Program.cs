5dbf9196acb84e297820d6a79ccfeaf6d7d17854
Vinicius <vinicius_mzo@hotmail.com>
2019-07-21 23:04:30 -0300

    Exercicio URI 1020

---
+++ 1020/1020/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _1020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            int[] tempo = new int[3] { 365, 30, 1 };
+            int[] resultado = new int[3];
+
+            idade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            for (int i = 0; i < 3; i++)
+            {
+                resultado[i] = idade / tempo[i];
+                idade = idade % tempo[i];
+            }
+
+
+            Console.WriteLine(resultado[0] + " ano(s)\n" + resultado[1] + " mes(es)\n" + resultado[2] + " dia(s)");
+            Console.ReadKey();
+        }
+    }
+}
