ae8f457802bad57714949df581cb7c87903d1051
Vinicius <vinicius_mzo@hotmail.com>
2019-07-24 14:47:01 -0300

    Exercicio URI 1037

---
+++ 1037/1037/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _1037
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor;
+
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (valor >= 0.00 && valor <= 25.00)
+            {
+                Console.WriteLine("Intervalo [0,25]");
+            }
+            if (valor > 25.00 && valor <= 50.00)
+            {
+                Console.WriteLine("Intervalo (25,50]");
+            }
+            if (valor > 50.00 && valor <= 75.00)
+            {
+                Console.WriteLine("Intervalo (50,75]");
+            }
+            if (valor > 75.00 && valor <= 100.00)
+            {
+                Console.WriteLine("Intervalo (75,100]");
+            }
+            if(valor < 0.00 || valor > 100.00) { Console.WriteLine("Fora de intervalo"); }
+
+            Console.ReadKey();
+        }
+    }
+}
