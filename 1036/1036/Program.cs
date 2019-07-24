de7b011d92ecbd4d6305bb448b4d533c5173f031
Vinicius <vinicius_mzo@hotmail.com>
2019-07-24 02:18:20 -0300

    Exercicio URI 1036

---
+++ 1036/1036/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _1036
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, delta, r1, r2;
+
+            String[] vet = Console.ReadLine().Split(' ');
+
+            a = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            b = double.Parse(vet[1], CultureInfo.InvariantCulture);
+            c = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            delta = Math.Pow(b, 2.0) - 4 * a * c;
+
+            if (a == 0.0 || delta < 0.0)
+            {
+                Console.WriteLine("Impossivel calcular");
+            }
+            else
+            {
+                r1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+                r2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+                Console.WriteLine("R1 = " + r1.ToString("F5", CultureInfo.InvariantCulture));
+                Console.WriteLine("R2 = " + r2.ToString("F5", CultureInfo.InvariantCulture));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
