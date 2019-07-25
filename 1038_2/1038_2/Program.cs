a3e5843a9e034f41f9b7ce683b03f0fdaa7a2f1f
Vinicius <vinicius_mzo@hotmail.com>
2019-07-25 14:33:18 -0300

    Exercicio URI 1038 (2)

---
+++ 1038_2/1038_2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _1038_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int codProduto, qtdProduto;
+            double total;
+
+            string[] vet = Console.ReadLine().Split(' ');
+
+            codProduto = int.Parse(vet[0], CultureInfo.InvariantCulture);
+            qtdProduto = int.Parse(vet[1], CultureInfo.InvariantCulture);
+
+            switch (codProduto)
+            {
+                case 1:
+                    total = qtdProduto * 4.00;
+                    Console.WriteLine("Total: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+                    break;
+                case 2:
+                    total = qtdProduto * 4.50;
+                    Console.WriteLine("Total: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+                    break;
+                case 3:
+                    total = qtdProduto * 5.00;
+                    Console.WriteLine("Total: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+                    break;
+                case 4:
+                    total = qtdProduto * 2.00;
+                    Console.WriteLine("Total: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+                    break;
+                case 5:
+                    total = qtdProduto * 1.50;
+                    Console.WriteLine("Total: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+                    break;
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
