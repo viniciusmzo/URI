61ecc9c40d991892885face01d92392a5a57f967
Vinicius <vinicius_mzo@hotmail.com>
2019-07-29 22:21:26 -0300

    Exercicio URI 1042

---
+++ 1042/1042/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1042
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] vet = Console.ReadLine().Split(' ');
+            List<int> listaOrdenada = new List<int>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                listaOrdenada.Add(int.Parse(vet[i]));
+            }
+
+            listaOrdenada.Sort();
+            foreach (int num in listaOrdenada)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(vet[i]);
+            }
+
+                Console.ReadKey();
+
+        }
+    }
+}
