2601368cdd11c7f0387bae6c08d0f3c2a0afeb25
Vinicius <vinicius_mzo@hotmail.com>
2019-07-30 12:01:26 -0300

    Exercicio URI 1046

---
+++ 1046/1046/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1046
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inicio, final, tempo;
+            string[] vet = Console.ReadLine().Split(' ');
+
+            inicio = int.Parse(vet[0]);
+            final = int.Parse(vet[1]);
+
+            if (final <= inicio) { final += 24; }
+            Console.WriteLine("O JOGO DUROU " + (final - inicio) +" HORA(S)");
+            Console.ReadKey();
+        }
+    }
+}
