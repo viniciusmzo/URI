20c367ba701da73c65e7e5617e78a4a51d1a6a52
Vinicius <vinicius_mzo@hotmail.com>
2019-07-29 21:14:41 -0300

    Exercicio URI 1041

---
+++ 1041/1041/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _1041
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            string[] vet = Console.ReadLine().Split(' ');
+
+            x = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            y = double.Parse(vet[1], CultureInfo.InvariantCulture);
+
+            if (x == 0 && y == 0)
+            {
+                Console.WriteLine("Origem");
+            }
+            else if (x == 0.00)
+            {
+                Console.WriteLine("Eixo Y");
+            }
+            else if (y == 0.00)
+            {
+                Console.WriteLine("Eixo X");
+            }
+            else if (x > 0.00 && y > 0.00)
+            {
+                Console.WriteLine("Q1");
+            }
+            else if (x < 0.00 && y > 0.00)
+            {
+                Console.WriteLine("Q2");
+            }
+            else if (x < 0.00 && y < 0.00)
+            {
+                Console.WriteLine("Q3");
+            }
+            else
+            {
+                Console.WriteLine("Q4");
+            }
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
