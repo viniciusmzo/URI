b39f39b8bb95dce01f9916d45a739ee7a57391fa
Vinicius <vinicius_mzo@hotmail.com>
2019-07-30 11:38:16 -0300

    Exercicio URI 1045

---
+++ 1045/1045/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _1045
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, temp;
+            string[] vet = Console.ReadLine().Split(' ');
+
+            a = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            b = double.Parse(vet[1], CultureInfo.InvariantCulture);
+            c = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            if (a < b)
+            {
+                temp = a;
+                a = b;
+                b = temp;
+            }
+            if (a < c)
+            {
+                temp = a;
+                a = c;
+                c = temp;
+            }
+            if (b < c)
+            {
+                temp = b;
+                b = c;
+                c = temp;
+            }
+
+
+            if (a >= (b + c))
+            {
+                Console.WriteLine("NAO FORMA TRIANGULO");
+            }
+            else
+            {
+                if (Math.Pow(a, 2) == (Math.Pow(b, 2) + Math.Pow(c, 2)))
+                {
+                    Console.WriteLine("TRIANGULO RETANGULO");
+                }
+                if (Math.Pow(a, 2) > (Math.Pow(b, 2) + Math.Pow(c, 2)))
+                {
+                    Console.WriteLine("TRIANGULO OBTUSANGULO");
+                }
+                if (Math.Pow(a, 2) < (Math.Pow(b, 2) + Math.Pow(c, 2)))
+                {
+                    Console.WriteLine("TRIANGULO ACUTANGULO");
+                }
+                if (a == b && a == c && b == c)
+                {
+                    Console.WriteLine("TRIANGULO EQUILATERO");
+                }
+                else if (a == b || a == c || b == c)
+                {
+                    Console.WriteLine("TRIANGULO ISOSCELES");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
