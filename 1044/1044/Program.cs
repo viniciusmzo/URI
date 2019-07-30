0f1d5bc736d7f5e3c4b00843a8cace6fb5a8b793
Vinicius <vinicius_mzo@hotmail.com>
2019-07-29 23:32:22 -0300

    Exercicio URI 1044

---
+++ 1044/1044/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1044
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorA, valorB;
+            string[] vet = Console.ReadLine().Split(' ');
+
+
+            valorA = int.Parse(vet[0]);
+            valorB = int.Parse(vet[1]);
+
+            if (valorA % valorB == 0 || valorB % valorA == 0)
+            {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
