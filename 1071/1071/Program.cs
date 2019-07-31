3ff6e7425c0b8bb81ce0ab5189f324a08db9710e
Vinicius <vinicius_mzo@hotmail.com>
2019-07-31 17:42:21 -0300

    Exercicio URI 1071

---
+++ 1071/1071/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1071
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, soma, aux;
+
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            soma = 0;
+
+            if (y < x)
+            {
+                aux = y;
+                y = x;
+                x = aux;
+            }
+
+            for (int i = (x + 1); i < y; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    soma = soma + i;
+                }
+            }
+            Console.WriteLine(soma.ToString());
+            Console.ReadKey();
+        }
+    }
+}
