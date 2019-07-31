d0e5e7674ddff4ca8d97119c016433c1f983644c
Vinicius <vinicius_mzo@hotmail.com>
2019-07-31 17:41:45 -0300

    Exercicio URI 1070

---
+++ 1070/1070/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1070
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, contador;
+
+            num = int.Parse(Console.ReadLine());
+            contador = 0;
+
+            while (contador < 6)
+            {
+                if (num % 2 != 0)
+                {
+                    Console.WriteLine(num);
+                    num += 1;
+                    contador += 1;
+                }
+                else { num += 1; }
+            }
+
+           
+            Console.ReadKey();
+        }
+    }
+}
