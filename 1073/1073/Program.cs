bc34d5c0dd20e74505fd6c4f1a25572209fcd89e
Vinicius <vinicius_mzo@hotmail.com>
2019-07-31 17:43:31 -0300

    Exercicio URI 1073

---
+++ 1073/1073/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1073
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, res;
+            
+            num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    res = (int)Math.Pow(i, 2);
+                    Console.WriteLine(i.ToString() + "^2 = " + res.ToString());
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
