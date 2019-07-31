6377402c805eff1c8e839bd0dfe2d68bb0f8a44b
Vinicius <vinicius_mzo@hotmail.com>
2019-07-31 17:39:45 -0300

    Exercicio URI 1065

---
+++ 1065/1065/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1065
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vet = new int[5];
+            int contador = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                vet[i] = int.Parse(Console.ReadLine());
+                if (vet[i] % 2 == 0)
+                {
+                    contador += 1;
+                }
+            }
+
+            Console.WriteLine(contador +" valores pares");
+            Console.ReadKey();
+        }
+    }
+}
