e5ff8bb93f3341419bc6eb7d969ef1281fde9c15
Vinicius <vinicius_mzo@hotmail.com>
2019-07-30 22:45:14 -0300

    Exercicio URI 1059

---
+++ 1059/1059/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1059
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i <= 100; i = i + 2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
