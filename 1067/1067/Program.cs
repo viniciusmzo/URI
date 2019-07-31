6958a2e72118930350ec3838b3653f7d43ea739f
Vinicius <vinicius_mzo@hotmail.com>
2019-07-31 17:41:10 -0300

    Exercicio URI 1067

---
+++ 1067/1067/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1067
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= num; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i.ToString());
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
