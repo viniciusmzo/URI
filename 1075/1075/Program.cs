767854ef1284a959aa6b1ad22ed7cc1035dd4ff5
Vinicius <vinicius_mzo@hotmail.com>
2019-08-06 19:57:53 -0300

    Exercicio URI 1075

---
+++ 1075/1075/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1075
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10000; i++)
+            {
+                if (i % n == 2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
