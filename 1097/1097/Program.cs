9be2e583251392cd31107c524126b630a4632024
Vinicius <vinicius_mzo@hotmail.com>
2019-08-07 15:40:42 -0300

    Exercicio URI 1097

---
+++ 1097/1097/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1097
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] resI = new int[5] { 1, 3, 5, 7, 9 };
+            int[] resJ = new int[5] { 7, 9, 11, 13, 15 };
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("I=" + resI[i] + " J=" + resJ[i]);
+                Console.WriteLine("I=" + resI[i] + " J=" + (resJ[i] - 1));
+                Console.WriteLine("I=" + resI[i] + " J=" + (resJ[i] - 2));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
