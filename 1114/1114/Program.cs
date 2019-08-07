ae4931871f26c5ba7550e02067af82444c705b62
Vinicius <vinicius_mzo@hotmail.com>
2019-08-06 23:15:20 -0300

    Exercicio URI 1114

---
+++ 1114/1114/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int senha;
+
+            senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Senha Invalida");
+                senha = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso Permitido");
+            Console.ReadKey();
+        }
+    }
+}
