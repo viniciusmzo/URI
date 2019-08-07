3f4f61d12d3fc5171e72de8d8e68020b374e65de
Vinicius <vinicius_mzo@hotmail.com>
2019-08-06 23:12:17 -0300

    Exercicio URI 1080

---
+++ 1080/1080/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1080
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int maior = 0, index = 0;
+            int[] vet = new int[100];
+
+            for (int i = 0; i < 100; i++)
+            {
+                n = int.Parse(Console.ReadLine());
+                vet[i] = n;
+                if (n > maior)
+                {
+                    maior = n;
+                    index = (Array.IndexOf(vet, maior) + 1);
+                }
+            }
+
+            Console.WriteLine(maior);
+            Console.WriteLine(index);
+            Console.ReadKey();
+        }
+    }
+}
