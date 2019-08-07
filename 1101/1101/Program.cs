efbcf7ad3ef8c6e859271aac7eef5340c40fb3a8
Vinicius <vinicius_mzo@hotmail.com>
2019-08-07 17:18:29 -0300

    Exercicio URI 1101

---
+++ 1101/1101/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m, n, aux;
+            string[] vet = Console.ReadLine().Split(' ');
+            m = int.Parse(vet[0]);
+            n = int.Parse(vet[1]);
+
+            while (m > 0 && n > 0)
+            {
+                int soma = 0;
+                if (m > n)
+                {
+                    aux = n;
+                    n = m;
+                    m = aux;
+                }
+
+                for (int i = m; i <= n; i++)
+                {
+                    Console.Write(i + " ");
+                    soma += i;
+                }
+                Console.WriteLine("Sum=" + soma);
+                soma = 0;
+
+                vet = Console.ReadLine().Split(' ');
+                m = int.Parse(vet[0]);
+                n = int.Parse(vet[1]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
