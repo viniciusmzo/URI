d3ae87596d25b8615b62674d101e86c76ccf1403
Vinicius <vinicius_mzo@hotmail.com>
2019-08-06 23:14:37 -0300

    Exercicio URI 1113

---
+++ 1113/1113/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            List<string> resultado = new List<string>();
+
+            do {
+                
+                string[] vet = Console.ReadLine().Split(' ');
+                x = int.Parse(vet[0]);
+                y = int.Parse(vet[1]);
+
+                if (x < y)
+                {
+                   resultado.Add("Crescente");
+                }
+                else if (x > y)
+                {
+                    resultado.Add("Decrescente");
+                }
+
+            } while (x != y);
+
+            foreach (string res in resultado)
+            {
+                Console.WriteLine(res);
+            }
+            Console.ReadKey();
+        }
+    }
+}
