5ca06a781c579222c8f80df0dad9ddbce51ecd8b
Vinicius <vinicius_mzo@hotmail.com>
2019-08-06 23:44:56 -0300

    Exercicio URI 1095

---
+++ 1095/1095/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1095
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] resI = new int[13];
+            int[] resJ = new int[13];
+            int valorI = 1;
+            int valorJ = 60;
+            
+
+            for (int i = 0; i < 13; i++)
+            {
+                resI[i] = valorI;
+                resJ[i] = valorJ;
+                valorI += 3;
+                valorJ -= 5;
+            }
+            for (int i = 0; i < 13; i++)
+            {
+                Console.WriteLine("I="+ resI[i] +" J=" + resJ[i]);
+            }
+
+                Console.ReadKey();
+        }
+    }
+}
