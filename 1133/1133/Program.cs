e2e1b42390ea5d82492d3a3259032fd97cfc8018
Vinicius <vinicius_mzo@hotmail.com>
2019-08-08 18:58:12 -0300

    Exercicio URI 1133

---
+++ 1133/1133/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1133
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            if (x < y)
+            {
+                for (int i = x + 1; i < y; i++)
+                {
+                    if (i % 5 == 2 || i % 5 == 3)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+            else
+            {
+                for (int i = y + 1; i < x; i++)
+                {
+                    if (i % 5 == 2 || i % 5 == 3)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
