713897b6915e21456427ea9e045abf183ef4f0c0
Vinicius <vinicius_mzo@hotmail.com>
2019-08-08 12:41:29 -0300

    Exercicio URI 1115

---
+++ 1115/1115/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            string[] vet = Console.ReadLine().Split(' ');
+
+            x = int.Parse(vet[0]);
+            y = int.Parse(vet[1]);
+
+            do
+            {
+                if (x == 0 || y == 0) break;
+                else if (x > 0 && y > 0)
+                {
+                    Console.WriteLine("primeiro");
+                }
+                else if (x < 0 && y > 0)
+                {
+                    Console.WriteLine("segundo");
+                }
+                else if (x < 0 && y < 0)
+                {
+                    Console.WriteLine("terceiro");
+                }
+                else
+                {
+                    Console.WriteLine("quarto");
+                }
+
+                vet = Console.ReadLine().Split(' ');
+
+                x = int.Parse(vet[0]);
+                y = int.Parse(vet[1]);
+
+            } while (x != 0 || y != 0) ;
+
+                Console.ReadKey();
+
+
+        }
+    }
+}
