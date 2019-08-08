f837e57ad6fe534d279f859926118b3e830c7642
Vinicius <vinicius_mzo@hotmail.com>
2019-08-08 13:10:17 -0300

    Exercicio URI 1117

---
+++ 1117/1117/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _1117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int notavalida = 0;
+            double nota, media = 0.0;
+
+            while (notavalida < 2)
+            {
+                nota = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (nota >= 0.0 && nota <= 10.0)
+                {
+                    media += nota;
+                    notavalida++;
+                }
+                else
+                {
+                    Console.WriteLine("nota invalida");
+                }
+            }
+            Console.WriteLine("media = " + (media /2).ToString("F2", CultureInfo.InvariantCulture));
+            Console.ReadKey();
+
+        }
+    }
+}
