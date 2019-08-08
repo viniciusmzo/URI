a01dd90f161a8a033b53875bc7a62ce1170bd143
Vinicius <vinicius_mzo@hotmail.com>
2019-08-08 20:39:27 -0300

    Exercicio URI 1142

---
+++ 1142/1142/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1142
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            num = int.Parse(Console.ReadLine());
+            num *= 4;
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (i % 4 != 0)
+                {
+                    Console.Write(i + " ");
+                }
+                else if (i % 4 == 0)
+                {
+                    Console.WriteLine("PUM");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
