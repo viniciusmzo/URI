23cebf195a9ed33540900cf70eb52222381839b1
Vinicius <vinicius_mzo@hotmail.com>
2019-08-08 20:46:19 -0300

    Exercicio URI 1143

---
+++ 1143/1143/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1143
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                Console.WriteLine(i + " " + (Math.Pow(i, 2)) + " " + (Math.Pow(i, 3)));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
