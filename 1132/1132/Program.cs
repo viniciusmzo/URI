0f54c7dd67869c4e006545cc83173b8b3bcd570a
Vinicius <vinicius_mzo@hotmail.com>
2019-08-08 18:22:56 -0300

    Exercicio URI 1132

---
+++ 1132/1132/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1132
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, soma = 0;
+
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            if (x > y)
+            {
+                var aux = y;
+                y = x;
+                x = aux;
+            }
+
+            for (int i = x; i <= y; i++)
+            {
+                if (i % 13 != 0)
+                {
+                    soma += i;
+                }
+            }
+
+            Console.WriteLine(soma);
+            Console.ReadKey();
+        }
+    }
+}
