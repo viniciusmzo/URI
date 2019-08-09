966c8b59abb2ecec80495ef1ab324b1ca7e158e4
Vinicius <vinicius_mzo@hotmail.com>
2019-08-09 17:47:21 -0300

    Exercicio URI 1144

---
+++ 1144/1144/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1144
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            n = int.Parse(Console.ReadLine());
+            
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i + " " + Math.Pow(i, 2) + " " + Math.Pow(i, 3));
+                Console.WriteLine(i + " " + (Math.Pow(i, 2) + 1) + " " + (Math.Pow(i, 3) + 1));
+                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
