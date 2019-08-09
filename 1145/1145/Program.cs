1d35fc9f887892610ac78e66cb959afb89a85872
Vinicius <vinicius_mzo@hotmail.com>
2019-08-09 18:38:59 -0300

    Exercicio URI 1145

---
+++ 1145/1145/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1145
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            x = int.Parse(vet[0]);
+            y = int.Parse(vet[1]);
+
+            for (int i = 1; i <= y; i++)
+            {
+
+                if (i % x != 0)
+                {
+                    Console.Write(i + " ");
+                }
+                else if (i % x == 0)
+                {
+                    Console.Write(i);
+                    Console.WriteLine();
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
