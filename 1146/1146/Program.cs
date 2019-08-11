9705d733a0c7a3540b9bedb1d5921cca1ccd161c
Vinicius <vinicius_mzo@hotmail.com>
2019-08-11 10:39:25 -0300

    Exercicio URI 1146

---
+++ 1146/1146/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1146
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            while (true)
+            {
+                num = int.Parse(Console.ReadLine());
+
+                if (num == 0) break;
+                else if (num > 0)
+                {
+                    Console.Write("1");
+                    for (int i = 2; i <= num; i++)
+                    {
+                        Console.Write(" " + i);
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
