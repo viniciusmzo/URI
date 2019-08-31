5de8836f9bce2b7424c1ce514cfbe0d5941ee0ab
Vinicius <vinicius_mzo@hotmail.com>
2019-08-30 23:23:29 -0300

    Exercicio URI 1154

---
+++ 1154/1154/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _1154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 0;
+            int idade;
+            double media = 0.0;
+
+            idade = int.Parse(Console.ReadLine());
+            while (idade > 0)
+            {
+                if (idade < 0) break;
+                else if (idade >= 0)
+                {
+                    media += idade;
+                    cont++;
+                }
+
+                idade = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine((media / cont).ToString("F2", CultureInfo.InvariantCulture));
+            Console.ReadKey();
+        }
+    }
+}
