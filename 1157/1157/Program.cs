960cfbe64fb6efe59edbf58454a9dbe1a9041212
Vinicius <vinicius_mzo@hotmail.com>
2019-09-01 00:25:26 -0300

    Exercicio URI 1157

---
+++ 1157/1157/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1157
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            n = int.Parse(Console.ReadLine());
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
