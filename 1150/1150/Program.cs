7dbbfb779c847369c04b679f2e107ad662d5c4a3
Vinicius <vinicius_mzo@hotmail.com>
2019-08-31 22:59:06 -0300

    Exercicio URI 1150

---
+++ 1150/1150/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1150
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, z, soma = 0, cont = 0;
+
+            x = int.Parse(Console.ReadLine());
+            z = int.Parse(Console.ReadLine());
+
+            while (z <= x)
+            {
+                z = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = x; i <= z; i++)
+            {
+                soma += i;
+                cont++;
+                if (soma > z) break;
+            }
+           
+
+            Console.WriteLine(cont);
+            Console.ReadKey();
+        }
+    }
+}
