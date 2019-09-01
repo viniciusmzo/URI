485113b3ae8f29e188f4d0777a2c2232077043f3
Vinicius <vinicius_mzo@hotmail.com>
2019-09-01 00:20:48 -0300

    Exercicio URI 1165

---
+++ 1165/1165/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1165
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, x, cont = 0;
+
+            x = int.Parse(Console.ReadLine());
+
+            for (int j = 0; j < x; j++)
+            {
+                n = int.Parse(Console.ReadLine());
+                for (int i = 1; i <= n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        cont++;
+                    }
+                }
+                if (cont > 2) { Console.WriteLine(n + " nao eh primo"); }
+                else if (cont <= 2) { Console.WriteLine(n + " eh primo"); }
+                cont = 0;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
