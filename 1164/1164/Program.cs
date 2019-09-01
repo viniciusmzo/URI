9e8d16251f4f63a5dce2df3f8b5e146c8c906c59
Vinicius <vinicius_mzo@hotmail.com>
2019-09-01 00:34:58 -0300

    Exercicio URI 1164

---
+++ 1164/1164/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1164
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, x, soma = 0;
+
+            x = int.Parse(Console.ReadLine());
+
+            for (int j = 0; j < x; j++)
+            {
+
+                n = int.Parse(Console.ReadLine());
+                for (int i = 1; i < n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        soma += i;
+                    }
+                }
+                if (soma == n) { Console.WriteLine(n + " eh perfeito"); }
+                else if (soma != n) { Console.WriteLine(n + " nao eh perfeito"); }
+                soma = 0;
+            }
+            Console.ReadKey();
+        }
+    }
+}
