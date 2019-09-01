2eec349ae3a91b94346f0930232280ce5dab46c2
Vinicius <vinicius_mzo@hotmail.com>
2019-09-01 00:04:58 -0300

    Exercicio URI 1153

---
+++ 1153/1153/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1153
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, fatorial;
+
+            n = int.Parse(Console.ReadLine());
+            fatorial = n;
+            for (int i = 1; i < n; i++)
+            {
+                fatorial *= i;
+            }
+
+            Console.WriteLine(fatorial);
+
+            Console.ReadKey();
+        }
+    }
+}
