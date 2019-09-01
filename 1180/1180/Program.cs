d316750c4d0844fdd71061bb18d9fa5f95af53bc
Vinicius <vinicius_mzo@hotmail.com>
2019-09-01 14:28:27 -0300

    Exercicio URI 1180

---
+++ 1180/1180/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1180
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, menor, aux = 0;
+            string[] dados;
+            int[] vet;
+
+
+            n = int.Parse(Console.ReadLine());
+            vet = new int[n];
+            dados = Console.ReadLine().Split(' ');
+            for (int i = 0; i < n; i++)
+            {
+                vet[i] = int.Parse(dados[i]);
+            }
+
+            menor = vet[0];
+
+            for (int i = 0; i < n; i++)
+            {
+                if (menor >= vet[i])
+                {
+                    menor = vet[i];
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if (menor == vet[i])
+                {
+                    aux = i;
+                    break;
+                }
+                
+            }
+
+            Console.WriteLine("Menor valor: " + menor);
+            Console.WriteLine("Posicao: " + aux);
+
+
+            Console.ReadKey();
+            
+        }
+    }
+}
