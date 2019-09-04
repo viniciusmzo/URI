089c91ea3d145d8fe7d9db0672a07645d41b46a3
Vinicius <vinicius_mzo@hotmail.com>
2019-09-04 15:17:21 -0300

    Exercicio URI 1155

---
+++ 1155/1155/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _1155
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cont = 1.00;
+            double soma = 0.00;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                soma += (1 / cont);
+                cont++;
+            }
+
+            Console.WriteLine(soma.ToString("F2", CultureInfo.InvariantCulture));
+            Console.ReadKey();
+        }
+    }
+}
