bb02f0e142b4daceb428d63f7c49d78f8bd4eca3
Vinicius <vinicius_mzo@hotmail.com>
2019-09-04 16:21:08 -0300

    Exercicio URI 1156

---
+++ 1156/1156/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace _1156
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b = 1, c, s = 0;
+
+            for (a = 1; a <= 39; a += 2)
+            {
+                c = a / b;
+                s += c;
+                b *= 2;
+            }
+
+
+            Console.WriteLine(s.ToString("F2", CultureInfo.InvariantCulture));
+            Console.ReadKey();
+        }
+    }
+}
