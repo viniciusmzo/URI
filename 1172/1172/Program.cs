1e3138544fbb83e05e864ed77e81609584a65f47
Vinicius <vinicius_mzo@hotmail.com>
2019-09-04 21:32:07 -0300

    Exercicio URI 1172

---
+++ 1172/1172/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1172
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int[] vet = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                n = int.Parse(Console.ReadLine());
+                vet[i] = n;
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (vet[i] <= 0)
+                {
+                    vet[i] = 1;
+                    Console.WriteLine("X[" + i + "] = " + vet[i]);
+                }
+                else if (vet[i] > 0)
+                {
+                    Console.WriteLine("X[" + i + "] = " + vet[i]);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
