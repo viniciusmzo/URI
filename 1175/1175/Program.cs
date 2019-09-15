061cf4a1f5c7effb177cbaedb6a064df64cd6ac1
Vinicius <vinicius_mzo@hotmail.com>
2019-09-14 21:53:43 -0300

    Exercicio URI 1175

---
+++ 1175/1175/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1175
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vet = new int[20];
+            int num, x = 19, aux;
+
+            for (int i = 0; i < 20; i++)
+            {
+                num = int.Parse(Console.ReadLine());
+                vet[i] = num;
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                aux = vet[i];
+                vet[i] = vet[x];
+                vet[x] = aux;
+                x--;
+            }
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("N[" + i + "] = " + vet[i]);
+            }
+
+                Console.ReadKey();
+        }
+    }
+}
