4ff05124e0553c36b56c47ad568f746a6ba291ef
Vinicius <vinicius_mzo@hotmail.com>
2019-09-14 21:24:57 -0300

    Exercicio URI 1173

---
+++ 1173/1173/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1173
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[10];
+            int x;
+
+            x = int.Parse(Console.ReadLine());
+
+            n[0] = x;
+
+            Console.WriteLine("N[0] = " + n[0]);
+            for (int i = 1; i < 10; i++)
+            {
+                n[i] = (n[i - 1] * 2);
+                Console.WriteLine("N["+ i +"] = " + n[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
