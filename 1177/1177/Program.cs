c3321da1a185f385f147595ff3964d45027230b8
Vinicius <vinicius_mzo@hotmail.com>
2019-09-14 22:06:23 -0300

    Exercicio URI 1177

---
+++ 1177/1177/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1177
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int[] vet = new int[1000];
+
+            num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < 1000; i++)
+            {
+                vet[i] = i % num;
+                Console.WriteLine("N["+ i +"] = " + vet[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
