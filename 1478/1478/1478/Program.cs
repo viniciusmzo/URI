44b25f2b5ece6d0bb2d22b53d75c2f50941d7c4c
Vinicius Marques <vinicius_mzo@hotmail.com>
2024-05-22 19:33:02 -0300

    URI 1478

---
+++ 1478/1478/1478/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _1478
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (num == 0)
+                {
+                    break;
+                }
+
+                int[,] matrix = new int[num, num];
+
+                for (int i = 0; i < num; i++) 
+                {
+                    for (int j = 0; j < num; j++) 
+                    {
+                        matrix[i, j] = Math.Abs(i - j) + 1;
+                    }
+                }
+
+                for (int i = 0; i < num; i++) 
+                {
+                    for (int j = 0; j < num; j++)
+                    {
+                        if (j == 0)
+                        {
+                            Console.Write(matrix[i, j].ToString().PadLeft(3));
+                        }
+                        else 
+                        {
+                            Console.Write(matrix[i, j].ToString().PadLeft(4));
+                        }
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
